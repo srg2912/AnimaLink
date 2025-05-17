510284ead5991df762cca7dfb31d10b2c00bd944
srg2912 <sergioalfonso_20@hotmail.com>
2025-05-17 19:54:00 +0200

    Added the possibility to choose looks and language.

--- src/Routers.mjs
+++ src/Routers.mjs
@@ -6,15 +6,18 @@ const router = Router();
 
 // POST Request to get character's personality
 router.post('/api/personality', async (req, res) => {
-    const { name, personality } = req.body;
+    const { name, personality, looks, language } = req.body; //Looks: male or female. Language: Spanish, English, Italian for now.
   
     if (!name || !personality) return res.status(400).json({ error: 'Name and personality are required.' });
   
     try {
       const prompt = `Create a detailed personality profile for a fictional character 
-      named "${name}". Their personality can be described as: ${personality}
-       Keep it concise and avoid using any markdown or formatting. Write in plain text, 
-       with no bullet points, bold text, numbered sections or new lines.
+      named "${name}". The character is a ${looks}.
+      Their personality can be described as: ${personality}. 
+      The character should speak ${language}.
+      Keep it concise and avoid using any markdown or formatting.
+      Write in plain text, with no bullet points, bold text, numbered sections or new lines.
+      Return you response in the same language the character speaks.
       `;
       const result = await ask_LLM(prompt);
       writeTo(result, './memory/personality.txt', 'w');
